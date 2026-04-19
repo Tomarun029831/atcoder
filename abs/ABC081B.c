7bee4122f5d9f34839404beb290eac5737773300
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-04-19 17:08:24 +0900

    refactor(abs): "Shift Only"

--- abs/ABC081B.c
+++ abs/ABC081B.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main(void) {
-  int n, i, j = 0;
+  int n, i, count = 0;
   scanf("%d", &n);
   int a[200];
   for (i = 0; i < n; i++)
     scanf("%d", a + i);
   while (1) {
     for (i = 0; i < n; i++) {
-      if (*(a + i) % 2 == 1) {
-        printf("%d\n", j);
+      if (*(a + i) & 1) {
+        printf("%d\n", count);
         return 0;
       }
-      *(a + i) /= 2;
+      *(a + i) >>= 1;
     }
-    ++j;
+    ++count;
   }
 }
