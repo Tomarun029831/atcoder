3460fdb3cf2b2b3c61f50fd6643834e4c9c804ea
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-04-19 21:35:25 +0900

    refactor(abs): bucket

--- abs/ABC085B.c
+++ abs/ABC085B.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int compare(const int *a, const int *b) { return *a - *b; }
 
 int main(void) {
-  int n, d[100], count = 1;
+  int n, d, bucket[101] = {0}, count = 0;
   scanf("%d", &n);
-  for (int i = 0; i < n; i++)
-    scanf("%d", d + i);
-  qsort(d, n, sizeof(int), (int (*)(const void *, const void *))compare);
-  for (int i = 0; i < n - 1; i++)
-    if (d[i] < d[i + 1])
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &d);
+    ++bucket[d];
+  }
+
+  for (int i = 1; i <= 100; i++)
+    if (bucket[i] > 0)
       ++count;
+
   printf("%d", count);
 
   return 0;
