f4f8c732badd9d90dbd55575592a35d363f6ff28
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-07-03 14:21:32 +0900

    perf(iron): O(n^2) -> O(n)

--- iron_rule_150/A06.c
+++ iron_rule_150/A06.c
@@ -2,15 +2,20 @@
 #include <stdlib.h>
 
 int main(){
-	int N, Q, *A;
+	int N, Q, *sum_A;
 	scanf("%d %d", &N, &Q);
-	A=calloc(N, sizeof(int)); 
-	for(int i=0; i<N; ++i) scanf("%d", A+i);
-	for(int i=0; i<Q; ++i){
-		int L,R,sum=0;
+	sum_A=calloc(N+1, sizeof(int)); 
+	if(sum_A == NULL) return 1;
+	sum_A[0]=0;
+	for(int i=1; i<N+1; ++i){
+		int input;
+		scanf("%d", &input);
+		sum_A[i] = sum_A[i-1] + input;
+	}
+	for(int i = 0; i<Q; ++i){
+		int L,R;
 		scanf("%d %d", &L, &R);
-		for(int j=L-1; j<R; ++j) sum += A[j];
-		printf("%d\n", sum);
+		printf("%d\n", sum_A[R] - sum_A[L-1]);
 	}
 
 	return 0;
