8c3c357aff9aef504d8b5b226d30156ad5d1b5f1
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-07-04 16:37:50 +0900

    perf(iron): 半分全列挙（Meet in the middle）

--- iron_rule_150/A14.c
+++ iron_rule_150/A14.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b) {
+    long long arg1 = *(const long long *)a;
+    long long arg2 = *(const long long *)b;
+    if (arg1 < arg2) return -1;
+    if (arg1 > arg2) return 1;
+    return 0;
+}
 
 long long table[4][1000];
+long long sum[2][1000*1000];
 int main(){
 	int N; long long K; scanf("%d %lld",&N,&K);
 	for(int i=0;i<4;++i)
 		for(int j=0;j<N;++j)
 			scanf("%lld",*(table+i)+j);
-	for(int i=0;i<N;++i) //A
-		for(int j=0;j<N;++j) //B
-			for(int k=0;k<N;++k) //C
-				for(int l=0;l<N;++l) //D
-					if(table[0][i]+table[1][j]+table[2][k]+table[3][l] == K){ puts("Yes");return 0; }
-	puts("No");
+	for(int i=0;i<N;++i)
+		for(int j=0;j<N;++j){
+			sum[0][i*N+j]=table[0][i]+table[1][j];
+			sum[1][i*N+j]=table[2][i]+table[3][j];
+		}
+	qsort(sum+1,N*N,sizeof(long long),compare);
+	for(int i=0;i<N*N;++i){
+		const long long diff = K-sum[0][i];
+		for(long long l=0,r=N*N-1,mid=(r+l)/2;l<=r;mid=(r+l)/2){
+			if(sum[1][mid]==diff) {puts("Yes"); return 0;}
+			else if(sum[1][mid]>diff) r=mid-1;
+			else l=mid+1;
+		}
+	}
 
+	puts("No");
 	return 0;
 }
