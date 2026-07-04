eb0c6307cfb9d9c46a220ac821d502c9b63210e5
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-07-05 01:44:28 +0900

    perf

--- iron_rule_150/A19.c
+++ iron_rule_150/A19.c
@@ -1,23 +1,15 @@
 #include <stdio.h>
-:wa
 
-long long dp[101][100001]; // dp[i][j]: iが種類数, 重さがちょうどjとなる時の最大価値
+long long dp[100001]; // dp[j]: 総重量がj以下であるような組み合わせで作れる最大価値
 int main(){
-	int N,W; scanf("%d %d", &N,&W);
-	for(int i=0;i<N+1;++i)
-		for(int j=0;j<W+1;++j)
-			dp[i][j]=-1000000000000000LL;
-	dp[0][0]=0;
-	for(int i=1;i<N+1;++i){
+	int N,W;scanf("%d %d",&N,&W);
+	for(int i=0;i<N;++i){
 		int w; long long v; scanf("%d %lld",&w,&v);
-		for(int j=0;j<W+1;++j){
-			if(j<w) dp[i][j] = dp[i-1][j];
-			else dp[i][j] = (dp[i-1][j]>(dp[i-1][j-w] + v)) ? dp[i-1][j] : (dp[i-1][j-w] + v);
+		for(int j=W;j>w-1;--j){
+			const long long next_v = dp[j - w] + v;
+			if (next_v > dp[j]) dp[j] = next_v;
 		}
 	}
-
-	long long max=0;
-	for(int i=0;i<W+1;++i) max = (dp[N][i] > max) ? dp[N][i] : max;
-	printf("%lld\n", max);
+	printf("%lld\n", dp[W]);
 	return 0;
 }
