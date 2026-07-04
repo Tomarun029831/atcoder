a7730c55299cba6a460e1a0148a76cd8d86a88d9
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-07-04 18:17:09 +0900

    fix: A16 is DP

--- iron_rule_150/A16.c
+++ iron_rule_150/A16.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
-int A[100001],B[100001];
+int A[100001],B[100001],dp[100001];
 int main(){
 	int N; scanf("%d",&N);
-	for(int i=2;i<N+1;++i) scanf("%d",A+i);
-	for(int i=3;i<N+1;++i) scanf("%d",B+i);
-	int sum=0;
-	for(int i=N;i>1;i-=(A[i]<B[i])?1:2){
-		sum+= (A[i]<B[i]) ? A[i] : B[i];
+	for(int i=2;i<N+1;++i) scanf("%d",A+i); // Ai: room i-1 to room i (2<=i<=N)
+	for(int i=3;i<N+1;++i) scanf("%d",B+i); // Bi: room i-2 to room i (3<=i<=N)
+
+	dp[1]=0;dp[2]=A[2]; // dp[i]は1からiへ移動するときの最小コスト
+	for(int i=3;i<N+1;++i){
+		const int pathA=dp[i-1]+A[i];
+		const int pathB=dp[i-2]+B[i];
+		dp[i]=(pathA<pathB) ? pathA : pathB;
 	}
 
-	printf("%d\n",sum);
+	printf("%d\n",dp[N]);
 	return 0;
 }
