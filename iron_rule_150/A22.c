7c52675e804d2da072f92d220227b1fa177c099c
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-07-05 12:36:14 +0900

    fix: but WA

--- iron_rule_150/A22.c
+++ iron_rule_150/A22.c
@@ -6,11 +6,19 @@ int dp[100001];	// dp[i]: マスiに到着したときの得点の最大値
 		// dp[B[i]] = max(dp[B[i]], dp[i] + 150)
 int main(){
 	int N; scanf("%d", &N);
-	for(int i=1;i<N+1;++i){
-		int A,B; scanf("%d %d",&A,&B);
-		dp[A] = (dp[A]>dp[i]+100) ? dp[A] : dp[i]+100;
-		dp[B] = (dp[B]>dp[i]+150) ? dp[B] : dp[i]+150;
+	for(int i=2;i<N+1;++i) dp[i] = -1;
+
+	for(int i=1;i<N;++i){
+		int A; scanf("%d",&A);
+		if(dp[i] == -1) continue;
+		dp[A] = (dp[A]>(dp[i]+100)) ? dp[A] : (dp[i]+100);
 	}
+	for(int i=1;i<N;++i){
+		int B; scanf("%d",&B);
+		if(dp[i] == -1) continue;
+		dp[B] = (dp[B]>(dp[i]+150)) ? dp[B] : (dp[i]+150);
+	}
+
 	printf("%d\n", dp[N]);
 	return 0;
 }
