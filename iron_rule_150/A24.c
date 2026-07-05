fae3b234f9e2576be0f5fe1bf5c429d5ae047a07
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-07-05 16:59:48 +0900

    feat: A24 but TLE

--- iron_rule_150/A24.c
+++ iron_rule_150/A24.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 
-int A[100001], dp[100001]; 
+int A[100001];
+int dfs(int i, int N){
+	int max=0;
+	for(int j=i+1;j<N+1;++j){
+		if(A[i]>=A[j]) continue;
+		const int temp = dfs(j, N);
+		if(temp>max) max = temp;
+	}
+	return max+1;
+}
+
 int main(){
-	int N; scanf("%d",&N);
+	int N,max=0; scanf("%d",&N);
 	for(int i=1; i<N+1; ++i) scanf("%d",A+i);
 
-
-
+	for(int i=1;i<N+1;++i){
+		const int temp = dfs(i, N);
+		if(temp>max) max = temp;
+	}
+	printf("%d\n",max);
 	return 0;
 }
