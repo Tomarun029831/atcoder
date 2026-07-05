1a47a43688f81cd2baac1b2065611de8061e52f6
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-07-05 10:29:50 +0900

    fix: but WA

--- iron_rule_150/A21.c
+++ iron_rule_150/A21.c
@@ -12,7 +12,7 @@ int max(int a, int b){ return (a>b)?a:b; }
 int P[2001], A[2001], dp[2001][2001];
 int main(){
 	int N; scanf("%d",&N);
-	for(int i=0; i<N; ++i) scanf("%d %d",P+i,A+i);
+	for(int i=1; i<N+1; ++i) scanf("%d %d",P+i,A+i);
 	for(int i=1;i<N;++i)
 		for(int j=i+1;j<N+1;++j)
 			dp[i][j]=max(dp[i+1][j] + ((i<P[i] && P[i]<j+1) ? A[i] : 0),
