f1f09854b4e5695b359e37b488ab6ebacf353e74
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-07-05 15:38:48 +0900

    fix: A23 難しい

--- iron_rule_150/A23.c
+++ iron_rule_150/A23.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
-int A[101][11], dp[101][11]; // dp[i][j]: 1~iのクーポンで、1~jの品物を取得可能なら1そうでないなら0
-			     // dp[i][j] = dp[i-1][j] ||		  // クーポンiを加えるときクーポン1~i-1で品物jまで買えるなら買える
-			     // 		(dp[i-1][j-1] && A[i][j]) // そうでないなら既に1~j-1まで変えていて追加するクーポンがjを買えるなら買える
+#define INF 101
+int A[101], dp[101][1<<10]; // dp[i][j]: 1~iのクーポンで、jのビットに対応するの品物を取得するときに必要なクーポンの枚数
+			    // dp[i][j] = min(dp[i][j], dp[i-1][j]) 		      クーポンを新たに加えないほうが最小枚数を構成できるならそうする
+			    // dp[i][j | A[i]] = min(dp[i-1][j] + 1, dp[i][j | A[i]]) 一枚追加するときそれを使用した方が最小枚数を構成できるならそうする
 int main(){
-	int N,M; scanf("%d %d",&N,&M);
-	for(int j=1;j<M+1;++j)
-		for(int i=1;i<N+1;++i)
-			scanf("%d", *(A+j)+i);
-	for(int i=0;i<M+1;++i) dp[i][0]=1;
-	for(int j=1;j<N+1;++j) dp[1][j]=(dp[1][j-1] && A[1][j]);
-
-	for(int i=2;i<M+1;++i)
-		for(int j=1;j<N+1;++j)
-			     dp[i][j] = (dp[i-1][j] || (dp[i-1][j-1] && A[i][j]));
+	int N,M; scanf("%d %d", &N,&M);
 	for(int i=1;i<M+1;++i)
-		if(dp[i][N]){ printf("%d\n",i); return 0; }
+		for(int j=0;j<N;++j){
+			int input; scanf("%d",&input);
+			A[i] |= (input << j);
+		}
 
-	printf("-1\n");
+	for(int i=0;i<M+1;++i)
+		for(int j=0;j<(1<<N);++j)
+			dp[i][j]=INF;
+	dp[0][0]=0;
+
+	for(int i=1;i<M+1;++i)
+		for(int j=0;j<(1<<N);++j){
+			dp[i][j] = (dp[i][j]<dp[i-1][j]) ? dp[i][j] : dp[i-1][j];
+			dp[i][j|A[i]] = (dp[i-1][j]+1 < dp[i][j|A[i]]) ? dp[i-1][j]+1 : dp[i][j|A[i]];
+		}
+
+	printf("%d\n", dp[M][(1<<N)-1] == INF ? -1 : dp[M][(1<<N)-1]);
 	return 0;
 }
