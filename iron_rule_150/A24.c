d3b991d8ba39fb5cd4a86edc8974b7ac332df3b7
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-07-05 19:39:09 +0900

    perf

--- iron_rule_150/A24.c
+++ iron_rule_150/A24.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 
-int A[100001], memo[100001];
-int dfs(int i, int N){
-	if(memo[i]!=0) return memo[i];
-	int max=0;
-	for(int j=i+1;j<N+1;++j){
-		if(A[i]>=A[j]) continue;
-		const int temp = dfs(j, N);
-		if(temp>max) max = temp;
-	}
-	memo[i]=max+1;
-	return max+1;
-}
+/*
+2 3 1 6 4 5に対して
+2
+2, 3
+1, 3, 6
+1, 3, 4
+1, 3, 4, 5
 
+dp[i]: 長さiとなるLISの末尾の最小値
+dp[0]=0
+dp[i] = min({A[j] | i<=j, dp[i-1]<A[j]})
+*/
+
+int A[100001], dp[100001];
 int main(){
-	int N,max=0; scanf("%d",&N);
+	int N; scanf("%d",&N);
 	for(int i=1; i<N+1; ++i) scanf("%d",A+i);
 
+	int len=0;
 	for(int i=1;i<N+1;++i){
-		const int temp = dfs(i, N);
-		if(temp>max) max = temp;
+		int l=0,r=len;
+		for(int mid=(l+r)/2;l<r;mid=(l+r)/2) // A[i]以上の値が初めて現れる境界を見つける
+			if(dp[mid]<A[i]) l=mid+1; else r=mid;
+		dp[l]=A[i];
+		if(l==len) ++len;
 	}
-	printf("%d\n",max);
+
+	printf("%d\n",len);
 	return 0;
 }
