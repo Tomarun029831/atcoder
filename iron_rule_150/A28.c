17a503d93ee891ea6214911eaabf3013cf11044a
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-07-05 23:55:33 +0900

    fix

--- iron_rule_150/A28.c
+++ iron_rule_150/A28.c
@@ -6,9 +6,12 @@ int main(){
 		getchar();
 		char T; int A; scanf("%c %d",&T,&A);	
 		if(T=='+') reg+=A;
-		else if(T=='-') reg-=A;
+		else if(T=='-') reg-=A; // 最悪でも0 ~ -100
 		else if(T=='*') reg*=A;
-		printf("%d\n",reg%10000);
+
+		if (reg < 0) reg += 10000; // 10000 ~ 9900
+		reg %= 10000; // (a mod M + b) mod M = a mod M + b mod M = (a+b) mod M
+		printf("%d\n",reg);
 	}
 
 	return 0;
