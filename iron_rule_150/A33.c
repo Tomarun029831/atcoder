bc94bc052de63cd0c8a7d072c68255fff1e262e2
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2026-07-06 18:08:47 +0900

    fix

--- iron_rule_150/A33.c
+++ iron_rule_150/A33.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
-long long A[100001], int dp[100001];
+/*
+https://ja.wikipedia.org/wiki/%E3%83%8B%E3%83%A0
+```
+コインの山の数を n とし、各山のコインの枚数を A1, …, An とする。
+S = A1 ⊕ ⋯ ⊕ An とおく。
+S ≠ 0 ならば先手必勝、S = 0 ならば後手必勝にできる。
+```
+*/
+
 int main(){
 	int N; scanf("%d",&N);
-	for(int i=1; i<N+1; ++i) scanf("%d",A+i);
-
+	int xor=0;
+	for(int i=1; i<N+1; ++i){
+		int input; scanf("%d",&input);
+		xor ^= input;
+	}
+	puts(xor ? "First" : "Second");
 	return 0;
 }
